ae4b53fbc96aa64985bcbdb537c0d3fed1cb184f
fassuncao <fabiola.assuncao@foursys.com.br>
2020-04-27 20:16:44 -0300

    correção ortografica e alteração de url de imagem

--- app/src/main/java/com/example/appfemmeit/PerfilService.kt
+++ app/src/main/java/com/example/appfemmeit/PerfilService.kt
@@ -11,12 +11,12 @@ object PerfilService {
             p.nome = "Usuario $i"
             p.cpf = "cpf $i"
             p.emaail = "email $i"
-            p.dtNascimento = "drNascimento $i"
-            p.linkedin = "limkedin $i"
+            p.dtNascimento = "dtNascimento $i"
+            p.linkedin = "linkedin $i"
             p.login = "login $i"
             p.telefone1 = "telefone1 $i"
             p.profissao = "profissao $i"
-            p.img = "https://img1.gratispng.com/20180326/cze/kisspng-women-s-empowerment-woman-international-women-s-da-women-day-5ab8d21502e510.9037538015220618450119.jpg"
+            p.img = "https://nailtime.com.br/wp-content/uploads/2018/11/00-Empoderamento-Feminino.jpg"
 
             perfil.add(p)
         }
