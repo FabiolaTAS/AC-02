031f7bbd7fb782053b53bb6fa2dcda7e623a5d4e
fassuncao <fabiola.assuncao@foursys.com.br>
2020-04-29 15:25:10 -0300

    exclusão

--- app/src/main/java/com/example/appfemmeit/TelaCadastroActivity.kt
+++ app/src/main/java/com/example/appfemmeit/TelaCadastroActivity.kt
@@ -28,8 +28,6 @@ class TelaCadastroActivity : AppCompatActivity() {
         val id = item?.itemId
         if (id == R.id.action_buscar) {
             Toast.makeText(this, "Clicou em buscar", Toast.LENGTH_LONG).show()
-        } else if (id == R.id.action_atualizar) {
-            Toast.makeText(this, "Clicou em atualizar", Toast.LENGTH_LONG).show()
         } else if (id == android.R.id.home) {
             finish()
         }
