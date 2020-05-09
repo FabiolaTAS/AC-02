bcec88115eb112d857734c8cd5a6a702f8e716be
fassuncao <fabiola.assuncao@foursys.com.br>
2020-05-09 18:50:43 -0300

    correção ortografica

--- app/src/main/java/com/example/appfemmeit/PerfilService.kt
+++ app/src/main/java/com/example/appfemmeit/PerfilService.kt
@@ -10,14 +10,14 @@ object PerfilService {
             val p = Perfil();
             p.nome = "Usuario $i"
             p.cpf = "cpf $i"
-            p.emaail = "email $i"
+            p.email = "email $i"
             p.dtNascimento = "dtNascimento $i"
             p.linkedin = "linkedin $i"
             p.login = "login $i"
             p.telefone1 = "telefone1 $i"
             p.profissao = "profissao $i"
             p.img = "https://nailtime.com.br/wp-content/uploads/2018/11/00-Empoderamento-Feminino.jpg"
-
+            p.perfil = "perfil $i"
             perfil.add(p)
         }
 
