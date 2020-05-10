2e27e7758b80621efae40e41010af4654234beee
fassuncao <fabiola.assuncao@foursys.com.br>
2020-05-09 22:54:22 -0300

    implementação service

--- app/src/main/java/com/example/appfemmeit/PerfilService.kt
+++ app/src/main/java/com/example/appfemmeit/PerfilService.kt
@@ -2,6 +2,10 @@ package com.example.appfemmeit
 
 import android.content.Context
 import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import java.lang.reflect.Type
 import java.net.URL
 
 object PerfilService {
@@ -11,13 +15,39 @@ object PerfilService {
     val TAG = "API-FEMMEIT"
 
     fun getPerfil(context: Context):List<Perfil>{
+        if(AndroidUtils.IsInternetOnline(context)) {
+            val url = "$host/perfil"
+            val json = HttpHelper.get(url)
 
-        val url = "$host/perfil"
-        val json = URL(url).readText()
+            Log.d(TAG, json)
 
+            return parserJson<List<Perfil>>(json)
+        }else{
+            return ArrayList()
+        }
+    }
+
+    fun toJson(): String{
+        return GsonBuilder().create().toJson(this)
+    }
+
+    fun save(perfil: Perfil): Response{
+        val json = HttpHelper.post("$host/perfil", perfil.toJson())
+        return parserJson<Response>(json)
+    }
+
+    fun delete(perfil: Perfil): Response {
+        Log.d(TAG, perfil.id.toString())
+        val url = "$host/perfil/${perfil.id}"
+        val json = HttpHelper.delete(url)
         Log.d(TAG, json)
+        return parserJson(json)
+    }
 
-        return ArrayList()
+    inline fun <reified T> parserJson(json: String): T {
+        val type = object : TypeToken<T>(){}.type
+        return Gson().fromJson<T>(json, type)
 
     }
+
 }
