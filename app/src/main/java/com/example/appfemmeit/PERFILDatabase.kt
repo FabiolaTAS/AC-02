9c82098604f24782f4a91cd11c0da5ebd9ac685b
fassuncao <fabiola.assuncao@foursys.com.br>
2020-05-17 20:43:58 -0300

    correção parte de persistencia.

--- app/src/main/java/com/example/appfemmeit/PERFILDatabase.kt
+++ app/src/main/java/com/example/appfemmeit/PERFILDatabase.kt
@@ -4,8 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(entities = arrayOf(Perfil::class), version = 1)
-abstract class PERFILDatabase: RoomDatabase(){
-
-    abstract fun perfilDAO(): PerfilDAO
+abstract class PERFILDatabase : RoomDatabase() {
+    abstract fun perfilDAO  (): PerfilDAO
 
 }
