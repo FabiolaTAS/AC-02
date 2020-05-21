b4fcd56a336cf175bd8da9ca44bf2182bf8ad347
fassuncao <fabiola.assuncao@foursys.com.br>
2020-05-21 12:06:28 -0300

    remoção de imports nao utilizados e identação de codigo

--- app/src/main/java/com/example/appfemmeit/NotificationUtil.kt
+++ app/src/main/java/com/example/appfemmeit/NotificationUtil.kt
@@ -1,12 +1,10 @@
 package com.example.appfemmeit
 
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.content.LocusId
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
@@ -27,11 +25,11 @@ object NotificationUtil {
         }
     }
 
-    fun create(context: Context, id: Int, intent: Intent, titulo: String, text: String){
+    fun create(context: Context, id: Int, intent: Intent, titulo: String, text: String) {
 
         createChannel(context)
 
-        val p = PendingIntent.getActivity(context,0,intent,PendingIntent.FLAG_UPDATE_CURRENT)
+        val p = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
         val builder = NotificationCompat.Builder(context, CHANNEL_ID)
             .setContentIntent(p)
@@ -41,10 +39,9 @@ object NotificationUtil {
             .setAutoCancel(true)
             .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
 
-        with(NotificationManagerCompat.from(context)){
+        with(NotificationManagerCompat.from(context)) {
             val n = builder.build()
-            notify(id,n)
+            notify(id, n)
         }
     }
-
 }
