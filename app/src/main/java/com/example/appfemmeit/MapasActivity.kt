00e83672bde86e92b678b636c5eb351aa13fbcf6
fassuncao <fabiola.assuncao@foursys.com.br>
2020-05-24 14:16:20 -0300

    implementação de validação de cpf

---
+++ app/src/main/java/com/example/appfemmeit/MapasActivity.kt
@@ -0,0 +1,21 @@
+package com.example.appfemmeit
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MapasActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mapas)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val mapaFragment = MapaFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.layoutMapas, mapaFragment)
+            .commit()
+    }
+}
